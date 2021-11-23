b15427fe3f36344a8c1aa70860a61d9445129f7d
partnernetsoftware <business@partnernetsoftware.com>
2021-11-23 14:14:06 +0800

    added superweb

---
+++ superweb.py
@@ -0,0 +1,18 @@
+from mypy import tryx,s2o,o2s
+
+class superweb:
+    def __init__(self,request=None):
+        self.request = request
+        param = {}
+        args = self.request.args or {}
+        for k in args: param[k] = args[k]
+
+        json_str = request.get_data()
+        #print('debug json_str',json_str)
+        if json_str:
+            json_obj = s2o(json_str) or {}
+            for k in json_obj: param[k] = json_obj[k]
+
+        self.param = param
+        #print('debug param',param)
+
