16e6b7c26c3feefc52f6a1dd76aaeb286246962c
partnernetsoftware <business@partnernetsoftware.com>
2021-11-27 13:06:54 +0800

    share py: time_maker()

--- src/mypy.py
+++ src/mypy.py
@@ -1,10 +1,15 @@
 #-*- coding: utf-8 -*-
 
 from __future__ import print_function
+_print= print
+_eval= eval
+_import= __import__
+
 
 def tryx(l,e=print):
     try: return l()
     except Exception as ex: return ex if True==e else e(ex) if e else None
+
 
 import json
 class MyJsonEncoder(json.JSONEncoder):
@@ -12,15 +17,27 @@ class MyJsonEncoder(json.JSONEncoder):
 s2o = lambda s:tryx(lambda:json.loads(s))
 o2s = lambda o,indent=None:tryx(lambda:json.dumps(o, indent=indent, ensure_ascii=False, cls=MyJsonEncoder))
 
+
+def time_maker(days=0,date=None,outfmt=None,infmt='%Y-%m-%d',months=0):
+    from datetime import datetime,timedelta
+    if date is None: _dt = datetime.now()
+    else: _dt = datetime.fromtimestamp(int(date)) if infmt=='0' or not infmt\
+         else datetime.strptime(str(date),infmt)
+    if months>0 or months<0:
+        from dateutil.relativedelta import relativedelta
+        _dt += relativedelta(months=months)
+    _dt += timedelta(days=days)
+    if outfmt is None: outfmt = infmt
+    if outfmt=='0' or not outfmt:return int(mktime(_dt.timetuple()))
+    return _dt.strftime(outfmt)
+
+
 import sys
 flag_py2 = sys.version_info.major==2
 if flag_py2: from urllib2 import urlopen
 else: from urllib.request import urlopen
 wc=lambda u=None, data=None, m='POST',timeout=10:tryx(lambda:urlopen(url=u,data=data.encode('utf-8') if isinstance(data,str) else o2s(data).encode('utf-8') if data else None,timeout=timeout).read().decode())
 
-_print= print
-_eval= eval
-_import= __import__
 
 void=lambda *ate:None
 
